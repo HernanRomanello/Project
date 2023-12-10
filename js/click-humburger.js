da3e990913d4f0867c78ee9b791de236a22afa40
HernanRomanello <143692103+HernanRomanello@users.noreply.github.com>
2023-12-10 07:59:25 +0200

    design-update

--- js/click-humburger.js
+++ js/click-humburger.js
@@ -4,6 +4,5 @@ const humburger = document.querySelector(".hamburger-button");
 
 humburger.addEventListener("click", () => {
   console.log("clicked");
-  // nav.classList.toggle("revealed-nav-list");
   nav.classList.toggle("revealed-nav-list");
 });
